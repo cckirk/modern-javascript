8056fc78dc1779c666ea803454b3075acb32f44d
Christopher Kirkwood <christopherkirkwood@Christophers-MacBook-Air.local>
2021-07-14 18:26:42 -0700

    adding dependencies

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 
-var moment = require('moment'); // require
-moment().format();
-
 console.log('hellooooooo');
 
 var math = require('mathjs');
 
-console.log('create and manipulate complex numbers')
-const a = math.complex(2, 3)
+console.log('create and manipulate complex numbers');
+const a = math.complex(2, 3);
 console.log(a); // 2 + 3i
 
 
 console.log('this is so fun!')
+
+function f(x, y = 12) {
+  // y is 12 if not passed (or passed as undefined)
+  return x + y;
+}
+console.log(f(3) === 15);
+
